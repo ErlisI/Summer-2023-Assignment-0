e155b98d4fbd2ce36ffeb745557802343853d8ac
Erlis Ibraimi <erloibri1@gmail.com>
2023-05-26 19:35:22 -0400

    titleCaseEdit - Done

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,9 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+    let s = title.split(" ");
+    for (let i = 0; i < s.length; i++)
+        s[i] = s[i].charAt(0).toUpperCase() + s[i].slice(1);
+    
+    return s.join(' ');
 }
 
 // Do not edit this line;
