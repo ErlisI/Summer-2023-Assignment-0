85642c79fdb72c8db04cdbd8feca2888791f78ee
Erlis Ibraimi <erloibri1@gmail.com>
2023-05-27 19:12:20 -0400

    isPalindrome - Done

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,8 @@
 function isPalindrome(word) {
-  // Insert code here;
+    temp = word.split("").reverse().join("");
+    if(word === temp)
+        return true;
+    return false;
 }
 
 // Do not edit this line;
