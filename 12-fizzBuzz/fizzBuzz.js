d193604fbcf0f0c85c4a3bf1ca0fd86cb86d5983
Erlis Ibraimi <erloibri1@gmail.com>
2023-05-28 19:04:31 -0400

    fizzBuzz - Done

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,16 @@
 function fizzBuzz(start, end) {
-  // Insert code here;
+  let ans = [];
+  for(let i = start; i <= end; i++){
+        if(i % 3 === 0 && i % 5 === 0)
+            ans.push("FizzBuzz");
+        else if(i % 3 === 0)
+            ans.push("Fizz");
+        else if(i % 5 === 0)
+            ans.push("Buzz");
+        else
+            ans.push(i);
+  }
+  return ans;
 }
 
 // Do not edit this line;
