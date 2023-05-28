5fd50e61706f6fd5a772237d7a704a79b3ef86c0
Erlis Ibraimi <erloibri1@gmail.com>
2023-05-28 19:36:56 -0400

    setUnionOfAnyAmountOfSets - Done

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,9 @@
 function setUnionOfAnyAmountOfSets(...args) {
-  // Insert code here;
+  let s = new Set();
+  for(let i of args)
+	for(let j of i)
+		s.add(j);
+  return s;
 }
 
 // Do not edit this line;
