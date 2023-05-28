d5ad89bcbb455a1c4558e08b3828bed0bd2f1f66
Erlis Ibraimi <erloibri1@gmail.com>
2023-05-28 19:20:24 -0400

    frequencyCounter - Done

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,12 @@
 function frequencyCounter(word) {
-  // Insert code here;
+  let freq = {};
+  for(let i = 0; i < word.length; i++){
+	if(freq[word[i]])
+		freq[word[i]] += 1;
+	else
+		freq[word[i]] = 1;
+   }
+  return freq;
 }
 
 // Do not edit this line;
