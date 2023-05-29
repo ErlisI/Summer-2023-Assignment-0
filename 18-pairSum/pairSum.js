d43439567f46d74421d5b6cfe78f9ffca1c5795b
Erlis Ibraimi <erloibri1@gmail.com>
2023-05-29 16:43:14 -0400

    18-pairSum - Done

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,17 @@
 function pairSum(nums, target) {
-  // Insert code here;
+  if(nums.length <= 1)
+	throw new Error('Length is < 1');
+  let p1 = 0;
+  let p2 = nums.length - 1;
+  for(let i = 0; i < nums.length; i++){
+	if((nums[p1] + nums[p2]) > target)
+		p2--;
+	else if((nums[p1] + nums[p2]) < target)
+		p1++;
+	else
+		return true;
+  }
+ return false;
 }
 
 // Do not edit this line;
